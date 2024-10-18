f589bab75ce519c840ee76f7065e6f67d4c0288f
Maria-Carolina-Rinaldi <dr.rinaldimaria@gmail.com>
2024-10-18 12:24:32 +0200

    additional tools and map page

---
+++ client/src/components/views/Tools.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import "../../App.css";
+import MapView from "../pages/MapView";
+// import TablesOption from "../pages/TablesOption";
+
+const Tools = () => {
+  const [view, setView] = useState(null);
+
+  return (
+    <div className="tools-container">
+    <h2>Additional Tools Simulator</h2>
+<MapView/>
+    </div>
+  );
+};
+
+export default Tools;
